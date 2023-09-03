ca23cbf3d011823cc1c56bd9f63f1b3904472478
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2023-09-03 15:15:06 +0530

    Icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/movix-logo.svg";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
     const [show, setShow] = useState("top");
