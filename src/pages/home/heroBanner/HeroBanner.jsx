ffb7722d2bf1d3b2af7519222aee87357fb82167
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2023-09-03 15:50:24 +0530

    Scss

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -39,7 +39,7 @@ const HeroBanner = () => {
             <div className="opacity-layer"></div>
             <ContentWrapper>
                 <div className="heroBannerContent">
-                    <span className="title">Welcome</span>
+                    <span className="title">MovieTimes</span>
                     <span className="subTitle">
                         Millions of Movies, TV shows and people to discover.
                         Explore now.
